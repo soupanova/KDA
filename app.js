697b1d323455fc2186d15fb563dc534e8c3520e2
Tommy Holt <tommy_holt@icloud.com>
2020-10-09 10:43:57 +0100

    Task 1 done. Database needed.

--- app.js
+++ app.js
@@ -2,27 +2,18 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const port = process.env.port || 5000;
+const {recipes} = require("./api/recipes")
 
 app.use(express.static("public"));
 app.use(express.json());
 
 // write a get request 
 
-app.get(, (req, res) {
-
+app.get("/api/recipes", async (req, res) => {
+  const allRecipes = await recipes()
+  console.log("Refreshed!");
+  res.json({payload: allRecipes})
 })
-
-const data = [
-  {
-    title: "Beans on Toast",
-    ingredients: ["150g of beans", "150g of butter", "150g of toast"],
-    instructions: `Put the butter in your mouth, wait 2 seconds to allow slight melting. Then follow with the toast. Swish around for 10-15 seconds to allow even coating of butter on the toast. Then add the beans, slowly.
-
-  Season to taste.`,
-    image:
-      "https://natashaskitchen.com/wp-content/uploads/2019/04/Best-Burger-4-500x375.jpg"
-  }
-];
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(`${__dirname}/views/index.html`));
@@ -31,4 +22,10 @@ app.get("/", function (req, res) {
 
 app.listen(port, () => {
   console.log(`App is listening, http://localhost:${port}`);
+})
+
+app.post("/api/recipes", async (req, res) => {
+  const allRecipes = await recipes()
+  allRecipes.push(req.body)
+  console.log(allRecipes)
 })
